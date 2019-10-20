5245daba813a22f5114fed2557fb05ab03314637
tmirun <tmirun@hotmail.com>
2019-10-20 18:08:43 +0200

    add all pages in routes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,14 +1,19 @@
 import { Welcome } from './pages/Welcome/Welcome';
 import React from 'react';
 import { Route } from 'react-router';
+import { Preparation } from './pages/Preparation/Preparation';
+import { Game } from './pages/Game/Game';
 
 export const ROUTE_WELLCOME_PATH = '/';
 export const ROUTE_PREPARATION_PATH = '/preparation';
+export const ROUTE_GAME_PATH = '/game';
 
 export const AppRoutes: React.FC = () => {
   return (
     <>
-      <Route path="/" component={Welcome} ></Route>
+      <Route path={ROUTE_WELLCOME_PATH} exact component={Welcome} ></Route>
+      <Route path={ROUTE_PREPARATION_PATH} component={Preparation} ></Route>
+      <Route path={ROUTE_GAME_PATH} component={Game} ></Route>
     </>
   );
 }
