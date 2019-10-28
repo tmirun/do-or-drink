182eaa42329decf2ffac7d2e9c9aea7fa7976809
tmirun <tmirun@hotmail.com>
2019-10-28 01:32:18 +0100

    save and get state from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
-import { AppRoutes } from './AppRoutes'
+import React, { useState, useEffect } from 'react';
+import { Switch, useHistory } from 'react-router-dom';
+import { AppRoutes, ROUTE_GAME_PATH } from './AppRoutes'
+import { store } from './stores/store';
+import { saveState, getState } from './apis/state.api';
+import { refreshAllState } from './actions/global.actions';
+import { useDispatch } from 'react-redux';
 
 import './App.scss';
 
 const App: React.FC = () => {
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  
+  useEffect(() => {
+    store.subscribe(() => {
+      saveState(store.getState())
+    })
+    const getStateFromServer = async () => {
+      try {
+        const state = await getState();
+        dispatch(refreshAllState(state));
+        if (state.game && state.game.start) {
+          history.push(ROUTE_GAME_PATH)
+        }
+      } catch (e) {
+        alert(`server error, some thing happen: ${e.message}`)
+      }
+      setLoading(false);
+    }
+    getStateFromServer();
+    // eslint-disable-next-line 
+  }, [])
+
   return (
     <div className="App">
-      <BrowserRouter>
-       <Switch>
-         <AppRoutes/>
-       </Switch>
-      </BrowserRouter>
+      {loading ? 
+        <div>LOADING</div> :
+        <Switch>
+          <AppRoutes/>
+        </Switch>
+      }
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { store } from './stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </Provider>
 , document.getElementById('root'));
 
